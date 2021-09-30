9b7f29097514c1058aab70cee257e6385b313b5b
sabin dangi <sabin4.dangi@gmail.com>
2021-09-29 20:28:31 -0700

    all files commit

---
+++ src/main/java/com/dangisabin/springpaypal/SpringpaypalApplication.java
@@ -0,0 +1,13 @@
+package com.dangisabin.springpaypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpaypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpaypalApplication.class, args);
+	}
+
+}
